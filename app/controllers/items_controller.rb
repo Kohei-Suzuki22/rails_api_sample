76762f49dec20250936caec00f12fdf0c7f15070
kohei <3173010@stu.hus.ac.jp>
2019-08-13 00:42:09 +0900

    make items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,37 @@
 class ItemsController < ApplicationController
+  before_action :set_todo
+  before_action :set_todo_item, only: :[:show, :update, :destroy]
+
+  def index
+    json_response(@todo.items)
+  end
+
+  def show
+    json_response(@items)
+  end
+
+  def create
+    json_response(@item)
+  end
+
+  def update
+    @item.update(item_params)
+    head :no_content
+  end
+
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :done)
+  end
+
+  def set_todo_item
+    @item = @todo.items.find_by!(id: params[:id]) if @todo
+  end
+
 end
